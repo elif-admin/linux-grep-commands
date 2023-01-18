7f3421025a14468a591d8c35c5f42fa70571dc6c
elif-admin <elifmalkoc55@gmail.com>
2023-01-18 22:58:16 +0000

    grep commands added

---
+++ grep.sh
@@ -0,0 +1,33 @@
+# boot
+# book
+# booze
+# machine
+# boots
+# bungie
+# bark
+# aardvark
+# broken$tuff
+# robots
+
+# grep "boo"
+# # grep -n "boo" grep-sed-awk.sh 
+# grep -v "boo" grep-sed-awk.sh 
+# grep -vn "boo" grep-sed-awk.sh #grep will print every line that does not contain the string "boo," and will display the
+# line numbers
+# grep -c "boo" grep-sed-awk.sh   #The -c option tells grep to supress the printing of matching lines, and only display the number of
+# lines that match the query
+# grep -l "boo" *  #print the filename,This is useful if you are searching through multiple files for the same string.
+# grep -i "BOO" #An option more useful for searching through non-code files is -i, ignore case
+# grep -x "boo" # the following command will print
+# nothing, because there are no lines that only contain the pattern "boo"
+# grep -w "boo"
+# grep "bo" *
+# grep "bo" grep-sed-awk.sh
+# grep "^ro" grep-sed-awk.sh #The `^` regular expression pattern specifies the start of a line
+# grep -A2 "mach" #Use -A and a number of lines to display after a match.
+# grep -B2 "mach" #Use -B and a number of lines to display before a match.
+# grep -C3 "mach" #Use -C and a number of lines to display before and after the match
+# grep "rk$"
+# grep -E "boots"
+# grep '\$'
+# grep Linux -r -l ~/(dosya ismi)linux kelimesinin hangi dosyalarda gectigini bulur
